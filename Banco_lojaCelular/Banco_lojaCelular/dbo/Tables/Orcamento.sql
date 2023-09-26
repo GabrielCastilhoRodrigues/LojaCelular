39a658b6259a96eb8ea1750b1f7572e67528061c
gabri <gabri@DESKTOP-LG003JH>
2023-09-25 21:19:25 -0300

    Adicionar arquivos de projeto.

---
+++ Banco_lojaCelular/Banco_lojaCelular/dbo/Tables/Orcamento.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Orcamento] (
+    [id_orcamento] INT             IDENTITY (1, 1) NOT NULL,
+    [id_cliente]   INT             NOT NULL,
+    [vlr_total]    DECIMAL (15, 6) NULL,
+    PRIMARY KEY CLUSTERED ([id_orcamento] ASC),
+    FOREIGN KEY ([id_cliente]) REFERENCES [dbo].[Cliente] ([id_cliente])
+);
+
---
+++ Banco_lojaCelular/Banco_lojaCelular/dbo/Tables/Venda.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Venda] (
+    [id_venda]     INT             IDENTITY (1, 1) NOT NULL,
+    [id_orcamento] INT             NOT NULL,
+    [vlr_total]    DECIMAL (15, 6) NULL,
+    [id_cliente]   INT             NOT NULL,
+    PRIMARY KEY CLUSTERED ([id_venda] ASC),
+    FOREIGN KEY ([id_cliente]) REFERENCES [dbo].[Cliente] ([id_cliente]),
+    FOREIGN KEY ([id_orcamento]) REFERENCES [dbo].[Orcamento] ([id_orcamento])
+);
+
